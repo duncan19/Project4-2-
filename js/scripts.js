59078e6a629159438fcca62198088765e3166f5c
Duncan Robbins <duncanrobbins19@gmail.com>
2019-11-08 13:39:32 -0800

    gather user inputs along with start making price log

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,33 @@
+var toppingsarray = []
+var price = 0
 $(document).ready(function(){
-
-
+  $("#pizzamakerform").submit(function(event){
+    event.preventDefault()
+//getting user's pizza size
+  var sizeinput = $("input[name='size']:checked").val();
+//getting their toppings
+$('input[name="toppings"]:checked').each(function(event) {
+  event.preventDefault()
+toppingsarray.push(this.value);
+});
+if (toppingsarray.legnth === 1){
+  var price = 0 + 1
+  console.log(price);
+}
+else if (toppingsarray.legnth === 2){
+  var price = 0 + 2
+console.log(price);
+}
+else if (toppingsarray.legnth === 3){
+  var price = 0 + 3
+console.log(price);
+}
+else if (toppingsarray.legnth === 4){
+  var price = 0 + 4
+console.log(price);
+}
+else{
+console.log(price);
+}
+});
 });
